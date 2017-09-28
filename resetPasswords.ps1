4ed99e89f760b4a73259ae551189022cd0509c63
Chris <chris.adale@gmail.com>
2017-09-28 11:04:17 +0200

    Initial committ

---
+++ resetPasswords.ps1
@@ -0,0 +1,26 @@
+$creds = Get-Credential
+
+/* Expand this into using https://github.com/berzerk0/Probable-Wordlists/tree/master/Real-Passwords, and also custom list based on keywords from the organisation. */
+$knownBad = "D7253A59D621A5E58CD7649C224B9FFD",
+			 "58A478135A93AC3BF058A5EA0E8FDB71"
+
+			 
+$hashes = Get-ADReplAccount -All -NamingContext 'DC=133host,DC=local' -server 192.168.56.200 -Credential $creds
+foreach($acc in $hashes) {
+  write-host "Working:" $acc.SamAccountName
+  if (![String]::isNullOrEmpty($acc.NTHash)) {
+	  $hash = [System.BitConverter]::ToString($acc.NTHash).replace("-","")
+	  foreach($h in $knownBad) {
+		if($h -eq $hash) {
+			write-host -ForeGroundColor "red" "WEAK PASSWORD. RESET!"
+			#AUTOMATIC PW RESET CODE
+		}
+	  } 
+  }
+}	
+
+
+
+function resetPassword($username) {
+	Set-User $username -ChangePasswordAtLogon $true 
+}
